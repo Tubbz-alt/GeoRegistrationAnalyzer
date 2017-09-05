33df6db2cd25a8f858d87c13ef953669b6267b5e
Marvin Smith <marvin_smith1@me.com>
2017-09-04 17:50:21 -0700

    Working on build options for Windows Systems.

---
+++ runner-options.ps1
@@ -0,0 +1,24 @@
+﻿
+function Add_CMake_Var( [string]$Key, [string]$Value )
+{
+	Write-Output  "-D${Key}=${Value}"
+}
+
+function Append_CMake_Var( [string]$Key, [string]$Value )
+{
+	Write-Output " $(Add_CMake_Var $Key $Value)"
+}
+
+#  Print the Boost-Root
+function Update_CMake_Args()
+{
+	$CMAKE_ARGLIST=""
+	$CMAKE_ARGLIST += $(Append_CMake_Var "BOOST_ROOT" "D:/opt/msvc-2017")
+	
+	Write-Output $CMAKE_ARGLIST
+}
+
+function Update_Env_Vars()
+{
+	$env:GDAL_ROOT="D:/opt/msvc-2017"
+}
---
+++ runner.ps1
@@ -0,0 +1,31 @@
+﻿
+
+Write-Output 'Running GeoRegistrationAnalyzer Build Utility.'
+$CMAKE_ARGLIST=""
+
+# Check if Build-Options subscript exists
+if( Test-Path ./runner-options.ps1 )
+{
+    Write-Output ' -> Importing Build Options'
+    . .\runner-options.ps1
+	$CMAKE_ARGLIST=Update_CMake_Args
+    Update_Env_Vars
+}
+else
+{
+    Write-Output ' -> Skipping Import of Runner Options'
+}
+
+
+#   Enter release directory
+if( -Not $(Test-Path ./release) )
+{
+    New-Item -ItemType Directory release
+}
+
+Set-Location release
+
+$CMD_ARGS="cmake $CMAKE_ARGLIST .."
+Invoke-Expression $CMD_ARGS 
+
+Set-Location ..
